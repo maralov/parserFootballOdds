aef8957d3c20ecf89452dacdcacacd89eb0fa645
m.aralov <m.aralov@smartweb.com.ua>
2023-12-07 21:26:51 +0200

    test_fly

--- csv.js
+++ csv.js
@@ -1,40 +1,28 @@
 const fs = require('fs');
 //
 function saveToCSV(data, filename) {
-	const header = 'Team Home,Team Away, Country, Match Score,First Half Score,Type,Home Odds Change,Draw Odds Change,Away Odds Change,URL,Prediction\n';
-	const f = data.filter(m => (m.droppingOdds.home + m.droppingOdds.away + m.droppingOdds.draw) / 3 < -2)
-	if (f.length === 0) {
+	const header = 'Team Home,Team Away, Country, Home Odds Change,Draw Odds Change,Away Odds Change,URL,Prediction\n';
+	if (data.length === 0) {
 		console.log('No matches to save');
 		return;
 	}
-	const rows = f.map(match => {
-		const droppingOdds = match.droppingOdds || {};
-		const matchScore = match.score ? `"${match.score.match}"` : '""';
-		const firstPartScore = match.score ? `"${match.score.firstPart}"` : '""';
-		const matchType = match.score ? `"${match.score.type}"` : '""';
+	const rows = data.map(match => {
 		const country = match.country !== undefined ? match.country : '""';
+		const prediction = match.prediction !== undefined ? match.prediction : '""';
 
-		let prediction = 'draw';
-
-		if (droppingOdds.away < -20) {
-			prediction = 'away';
-		}
-		if (droppingOdds.home < -30) {
-			prediction = 'home';
-		}
-
-		return `"${match.teamHome}","${match.teamAway}",${country},${matchScore},${firstPartScore},${matchType},${match.droppingOdds.home},${match.droppingOdds.draw},${match.droppingOdds.away},"${match.url}", ${prediction}`;
+		return `"${match.teamHome}","${match.teamAway}",${country},${match.droppingOdds.home},${match.droppingOdds.draw},${match.droppingOdds.away},"${match.url}", ${prediction}`;
 	}).join('\n');
 
     fs.writeFileSync(filename, header + rows);
 }
 
-function importAndSaveData(jsonFile, csvBaseFile) {
-	const rawData = fs.readFileSync(jsonFile);
-	const data = JSON.parse(rawData);
+function importAndSaveData() {
 	const today = new Date();
-	const dateString = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
-	const csvFile = `./results/bets/${dateString}.csv`;
+	const dateString = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`
+
+	const rawData = fs.readFileSync(`./results/${dateString}.json`);
+	const data = JSON.parse(rawData);
+	const csvFile = `./csv/${dateString}.csv`;
 	saveToCSV(data, csvFile);
 }
 
--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
-
+const path = require('path');
+require('dotenv').config();
+const axios = require('axios');
 const userAgent = require('./helpers/constants');
 const os = require("os");
-
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
 const BASE_URL = 'https://www.flashscore.com/football/';
 const leagues = [
 	'austria/admiral-bundesliga',
 	'england/premier-league',
+	'england/championship',
 	'spain/laliga',
 	'italy/serie-a',
 	'germany/bundesliga',
+	'greece/super-league',
+	'poland/ekstraklasa',
+	'slovakia/nike-liga',
+	'slovenia/prva-liga',
 	'france/ligue-1',
 	'belgium/jupiler-league',
 	'norway/eliteserien',
 	'denmark/superliga/',
 	'netherlands/eredivisie',
 	'belgium/jupiler-pro-league',
-	'europe/champions-league',
-	'europe/europa-league',
 ];
 
 let executablePath;
@@ -28,117 +34,227 @@ if (os.platform() === 'linux') {
 	executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 }
 
-async function filterMatchByOdds(page, match, range = 2) {
-	await page.goto(match.url, {waitUntil: 'networkidle2', timeout: 60000});
 
-	const odds = await page.evaluate(() => {
-		const odds1 = document.querySelector('.o_1 .oddsValueInner')?.textContent;
-		const odds2 = document.querySelector('.o_2 .oddsValueInner')?.textContent;
+// Функція для визначення часу запуску на основі зібраних даних
+function planScrapingBasedOnMatches(matches) {
+	const uniqueTimesSet = new Set();
 
-		return {
-			odds1: parseFloat(odds1),
-			odds2: parseFloat(odds2),
-		};
+	matches.forEach(match => {
+		const today = new Date();
+		const [hours, minutes] = match.date.split(':').map(Number);
+		const matchDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hours, minutes);
+
+		// Додавання тільки унікальних часів до Set
+		uniqueTimesSet.add(matchDate.toISOString());
 	});
 
-	if (odds.odds1 >= range && odds.odds2 >= range) {
-		return match;
+	// Конвертація Set у масив та сортування
+	const sortedUniqueTimes = Array.from(uniqueTimesSet)
+		.map(timeStr => new Date(timeStr))
+		.sort((a, b) => a - b);
+
+	// Записуємо графік запуску в файл
+	const schedulePath = path.join(__dirname, 'schedule.json');
+	fs.writeFileSync(schedulePath, JSON.stringify(sortedUniqueTimes, null, 2));
+}
+
+async function checkPredictions(page, path= '') {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const dateString = `${(yesterday.getMonth() + 1).toString().padStart(2, '0')}-${yesterday.getDate().toString().padStart(2, '0')}-${yesterday.getFullYear()}`;
+  const filePath = path ? `./results/${path}` : `./results/${dateString}.json`;
+
+  if (!fs.existsSync(filePath)) {
+    console.log("Немає файлу з прогнозами за вчора.");
+    return;
+  }
+
+  const matchesData = JSON.parse(fs.readFileSync(filePath));
+  console.log(`check yesterday predictions start... ${yesterday.toLocaleString()}`);
+
+  for (const match of matchesData) {
+  	if(match.result && !path) continue;
+    try {
+      await page.goto(match.url, { waitUntil: 'networkidle2', timeout: 60000 });
+
+      const score = await page.evaluate(() => {
+        const scoreElements = document.querySelectorAll('.duelParticipant__score .detailScore__wrapper span');
+        const scoreElementsFullTime = document.querySelectorAll('.duelParticipant__score .detailScore__fullTime span');
+		const scoreBlock = scoreElementsFullTime && scoreElementsFullTime.length > 0 ? scoreElementsFullTime : scoreElements;
+        return scoreBlock.length === 3 ? `${scoreBlock[0].textContent}-${scoreBlock[2].textContent}` : null;
+      });
+
+	  const odds = await page.evaluate(() => {
+		  const odd1 = document.querySelector('.odds .cell.o_1 .oddsValueInner')?.textContent;
+		  const oddx = document.querySelector('.odds .cell.o_0 .oddsValueInner')?.textContent;
+		  const odd2 = document.querySelector('.odds .cell.o_2 .oddsValueInner')?.textContent;
+
+		  return {home: parseFloat(odd1), draw:parseFloat(oddx), away: parseFloat(odd2)};
+	  })
+
+      if (score) {
+        const scoreType = score.split('-').map(Number).reduce((a, b) => a === b ? 'draw' : (a > b ? 'home' : 'away'));
+        const result = match.prediction === scoreType ? 'win' : 'lose';
+        console.log(`Yesterday match prediction ${match.prediction}: ${result}(${scoreType})`);
+        match.score = { fullTime: score, type: scoreType };
+        match.result = result;
+		match.odds = odds;
+      }
+    } catch (error) {
+      console.error(`Error checking match ${match.id}:`, error);
+    }
+  }
+  // Оновлення файлу з результатами
+  fs.writeFileSync(filePath, JSON.stringify(matchesData, null, 2));
+}
+
+async function updateAllPredictions(page) {
+	const resultsDir = path.join(__dirname, 'results');
+	console.log(`resultsDir:`, resultsDir)
+	const files = fs.readdirSync(resultsDir);
+	for (const file of files) {
+		console.log(`check file:`, file)
+		await checkPredictions(page, file);
 	}
 }
 
 async function scrapeLeagueData(page, leagueUrl) {
-	await page.goto(`${BASE_URL}${leagueUrl}`, {
-		waitUntil: 'networkidle2',
-		timeout: 60000,
-	});
-
-	const leagueName = leagueUrl ? leagueUrl : `All ${new Date().toLocaleString()} matches`
-
-	console.log(`Get summary from ${leagueName}...${new Date().toLocaleString()}`);
-
-	const matches = await page.evaluate((league) => {
-		const results = [];
-		const eventBlocks = document.querySelectorAll('.leagues--live .event__match--scheduled');
-		const country = league.split('/')[0];
-		if (eventBlocks.length === 0) {
-			console.log(`No matches in the league (${country}) today.`);
-			return results;
-		}
-
-		eventBlocks.forEach((block) => {
-			const id = block.getAttribute('id').replace('g_1_', '');
-			const teamHome = block.querySelector('.event__participant--home')?.textContent.trim();
-			const teamAway = block.querySelector('.event__participant--away')?.textContent.trim();
-			const date = block.querySelector('.event__time')?.textContent;
-
-			results.push({
-				country,
-				id,
-				date,
-				teamHome,
-				teamAway,
-				url: `https://www.flashscore.com/match/${id}/`,
-			});
+	try {
+		await page.goto(`${BASE_URL}${leagueUrl}`, {
+			waitUntil: 'networkidle2',
+			timeout: 60000,
 		});
 
-		return results;
-	}, leagueName);
+		const leagueName = leagueUrl ? leagueUrl : `All ${new Date().toLocaleString()} matches`
 
-	console.log(`Collect matches: `, matches.length);
+		console.log(`Get summary from ${leagueName}...${new Date().toLocaleString()}`);
 
-	const filteredMatches = [];
+		const matches = await page.evaluate(() => {
+			const results = [];
+			const eventBlocks = document.querySelectorAll('.leagues--live .event__match--scheduled');
+			if (eventBlocks.length === 0) {
+				console.log(`No matches in the league today.`);
+				return results;
+			}
 
-	for (let i = 0; i < matches.length; i++) {
-		const match = matches[i];
-		console.log(`Parsing match ${i + 1}/${matches.length} (${((i + 1) / matches.length * 100).toFixed(2)}%)`);
+			eventBlocks.forEach((block) => {
+				const id = block.getAttribute('id').replace('g_1_', '');
+				const teamHome = block.querySelector('.event__participant--home')?.textContent.trim();
+				const teamAway = block.querySelector('.event__participant--away')?.textContent.trim();
+				const date = block.querySelector('.event__time')?.textContent;
+				if (date) {
+					results.push({
+						id,
+						date: /(\d{2}:\d{2})/.exec(date)[1],
+						teamHome,
+						teamAway,
+						url: `https://www.flashscore.com/match/${id}/`,
+					});
+				};
 
-		const filteredMatch = await filterMatchByOdds(page, match,);
+			});
 
-		if (filteredMatch) {
+			return results;
+		});
+		planScrapingBasedOnMatches(matches);
+		console.log(`Collect matches: `, matches.length);
+
+		const filteredMatches = [];
+
+		for (let i = 0; i < matches.length; i++) {
+			const match = matches[i];
+			console.log(`Parsing match ${i + 1}/${matches.length} (${((i + 1) / matches.length * 100).toFixed(2)}%)`);
+
 			await page.goto(`${match.url}/#/odds-comparison/1x2-odds/full-time`, {
 				waitUntil: 'networkidle2',
 				timeout: 60000,
 			});
-			const droppingOdds = await page.evaluate(() => {
-				const processOddsChange = (title) => {
-					const odds = title.split(' » ').map(Number);
-					if (odds.length === 2 && !isNaN(odds[0]) && !isNaN(odds[1])) {
-						console.log(`odds:`, (odds[1] - odds[0]) / odds[0] * 100);
-						return (odds[1] - odds[0]) / odds[0] * 100;
-					}
+
+			const filteredMatch = await page.evaluate((matchItem) => {
+				const range = 2;
+				const odds1 = document.querySelector('a.oddsCell__odd:nth-child(2) span')?.textContent;
+				const odds2 = document.querySelector('a.oddsCell__odd:nth-child(4) span')?.textContent;
+
+
+				if (parseFloat(odds1) >= range && parseFloat(odds2) >= range) {
+					const country = document.querySelector('.tournamentHeader__country')?.textContent.toLowerCase().split('-')[0];
+					return {...matchItem, country};
+				} else {
 					return null;
-				};
+				}
 
-				const averageChange = (changes) => {
-					const total = changes.reduce((acc, change) => acc + change, 0);
-					return (Math.round(total / changes.length));
-				};
+			}, match)
 
-				const oddsChanges = Array.from(document.querySelectorAll('.ui-table__body .ui-table__row')).map(row => {
-					const homeChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(2)')?.getAttribute('title'));
-					const drawChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(3)')?.getAttribute('title'));
-					const awayChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(4)')?.getAttribute('title'));
-					return {homeChange, drawChange, awayChange};
+
+
+			if (filteredMatch) {
+				const droppingOdds = await page.evaluate(() => {
+					const processOddsChange = (title) => {
+						const odds = title.split(' » ').map(Number);
+						if (odds.length === 2 && !isNaN(odds[0]) && !isNaN(odds[1])) {
+							console.log(`odds:`, (odds[1] - odds[0]) / odds[0] * 100);
+							return (odds[1] - odds[0]) / odds[0] * 100;
+						}
+						return null;
+					};
+
+					const averageChange = (changes) => {
+						const total = changes.reduce((acc, change) => acc + change, 0);
+						return (Math.round(total / changes.length));
+					};
+
+					const oddsChanges = Array.from(document.querySelectorAll('.ui-table__body .ui-table__row')).map(row => {
+						const homeChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(2)')?.getAttribute('title'));
+						const drawChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(3)')?.getAttribute('title'));
+						const awayChange = processOddsChange(row.querySelector('a.oddsCell__odd:nth-child(4)')?.getAttribute('title'));
+						return {homeChange, drawChange, awayChange};
+					});
+
+					if (oddsChanges.length === 0) return '';
+					console.log(`oddsChanges:`, oddsChanges)
+					return {
+						home: averageChange(oddsChanges.map(change => change.homeChange)),
+						draw: averageChange(oddsChanges.map(change => change.drawChange)),
+						away: averageChange(oddsChanges.map(change => change.awayChange))
+					};
 				});
 
-				console.log(`oddsChanges:`, oddsChanges)
+				const advDropping = (droppingOdds.home + droppingOdds.away + droppingOdds.draw) / 3;
+				let prediction = '';
 
-				if (oddsChanges.length === 0) return '';
+				if (advDropping < -2) {
+					prediction = 'draw'
+					if (droppingOdds.away < -20) {
+						prediction = 'away';
+					}
+					if (droppingOdds.home < -30) {
+						prediction = 'home';
+					}
+				} else if (advDropping > 0 & droppingOdds.home <= 0 && droppingOdds.draw <= 0) {
+					prediction = 'draw';
+				} else if (advDropping > 2 && droppingOdds.draw > 1) {
+					prediction = 'draw';
+				} else if (advDropping >= -2  && advDropping < 0.5 && droppingOdds.home >= -10 && droppingOdds.home <= 10 && droppingOdds.draw <= 0 && droppingOdds.draw >= -4 && droppingOdds.away < -1) {
+					prediction = 'draw';
+				}
 
-				return {
-					home: averageChange(oddsChanges.map(change => change.homeChange)),
-					draw: averageChange(oddsChanges.map(change => change.drawChange)),
-					away: averageChange(oddsChanges.map(change => change.awayChange))
-				};
-			});
-
-			filteredMatch.droppingOdds = droppingOdds;
-
-			filteredMatches.push(filteredMatch);
+				if (prediction) {
+					filteredMatch.droppingOdds = droppingOdds;
+					filteredMatch.droppingOdds.advDropping = advDropping;
+					filteredMatch.prediction = prediction;
+					console.log(`find match: `, prediction)
+					filteredMatches.push(filteredMatch);
+				}
+			}
 		}
+
+		console.log(`filtered matches `, filteredMatches.length);
+
+		return filteredMatches;
+	} catch (error) {
+		console.error('Error scraping data:', error);
+		return [];
 	}
-	console.log(`filtered matches `, filteredMatches.length);
-	return filteredMatches;
 }
 
 async function scrapeData({all: all = false}) {
@@ -146,7 +262,7 @@ async function scrapeData({all: all = false}) {
 		executablePath,
 		headless: 'new',
 	});
-	console.log(`Statring scraping... ${new Date().toLocaleString()}`);
+
 	const page = await browser.newPage();
 	await page.setRequestInterception(true)
 	page.on('request', request => {
@@ -156,7 +272,12 @@ async function scrapeData({all: all = false}) {
 			request.continue();
 		}
 	});
+
+	await checkPredictions(page);
+
 	let allMatches = [];
+
+	console.log(`Statring scraping... ${new Date().toLocaleString()}`);
 
 	if (all) {
 		await page.setUserAgent(userAgent);
@@ -172,16 +293,33 @@ async function scrapeData({all: all = false}) {
 		}
 	}
 
-	console.log(`done! add ${allMatches.length}/${new Date().toLocaleString()}`);
+	console.log(`done! add ${allMatches.length} ... ${new Date().toLocaleString()}`);
 
 	await browser.close();
 	return allMatches;
 }
 
 function saveDataToFile(data) {
+	if (data.length === 0) return;
 	const today = new Date();
 	const dateString = `${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}-${today.getFullYear()}`;
-	fs.writeFile(`./results/${dateString}.json`, JSON.stringify(data, null, 2), (err) => {
+	const filePath = `./results/${dateString}.json`;
+
+	let existingData = [];
+	if (fs.existsSync(filePath)) {
+		const rawData = fs.readFileSync(filePath);
+		existingData = JSON.parse(rawData);
+	}
+
+	const uniqueData = [...existingData, ...data].reduce((acc, match) => {
+		const existingMatchIndex = acc.findIndex(m => m.id === match.id);
+		if (existingMatchIndex === -1) {
+			acc.push(match);
+		}
+		return acc;
+	}, []);
+
+	fs.writeFile(filePath, JSON.stringify(uniqueData, null, 2), (err) => {
 		if (err) {
 			console.error('Помилка при збереженні даних:', err);
 			return;
@@ -190,8 +328,43 @@ function saveDataToFile(data) {
 	});
 }
 
-scrapeData({all: true})
-	.then((filteredData) => {
-		saveDataToFile(filteredData);
-	})
-	.catch(console.error);
+async function sendTelegramMessage(matches=[]) {
+	const telegramApiUrl = `https://api.telegram.org/bot${token}/sendMessage`;
+	let messageText = `Сегодня матчи:\n`;
+	matches.forEach((match, index) => {
+		messageText += `${index + 1}. 🕐 ${match.date}, ${match.teamHome} - ${match.teamAway}(${match.country}) \n Прогноз: ${match.prediction} \n ${match.url}\n`;
+	});
+	console.log(matches)
+	try {
+		await axios.post(telegramApiUrl, {
+			chat_id: chatId,
+			text: messageText,
+			parse_mode: 'Markdown'
+		});
+		console.log('Message sent to Telegram');
+	} catch (error) {
+		console.error('Error sending message to Telegram:', error);
+	}
+}
+
+function isWeekday() {
+	const dayOfWeek = new Date().getDay();
+	// Понедельник = 1, вторник = 2, ..., воскресенье = 0 или 7
+	return dayOfWeek >= 1 && dayOfWeek <= 5;
+}
+
+function scrapeDataBasedOnDay() {
+	const all = isWeekday();
+	console.log(`Today scraping: ${all ? 'all' : 'top'} matches`)
+	scrapeData({all})
+		.then( data => {
+			saveDataToFile(data);
+			if (data.length > 0) {
+				sendTelegramMessage(data).then(console.log).catch(console.error);
+			}
+		})
+		.catch(console.error);
+}
+
+scrapeDataBasedOnDay();
+
